5cf822ef55298200453c18e3e76d276859776df2
Heru Handika <herubiolog@gmail.com>
2022-10-19 17:54:12 -0500

    Add intro R.

---
+++ R/intro.R
@@ -0,0 +1,11 @@
+# Title: Pengenalan Variabel dan Operator Aritmatika
+# Author: Heru Handika
+# Date created: 16 Oktober 2022
+
+## Operasi aritmatika
+2 + 2
+
+## Variable 
+hello <- "Hello World"
+
+hello2 = "Hello world"
