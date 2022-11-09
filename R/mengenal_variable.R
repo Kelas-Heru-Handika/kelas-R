e8c527ce3d9bc414806f4628dff24ac235c172b6
Heru Handika <herubiolog@gmail.com>
2022-11-09 08:56:14 -0600

    Add the first course.

---
+++ R/mengenal_variable.R
@@ -0,0 +1,32 @@
+# Ini pertambahan 5 + 5
+5 + 5
+
+# Ini perkalian 2 * 2
+2 * 2
+
+2 ^ 2
+
+# snake_case
+variabel_pertama
+
+# camelCase
+variabelPertama
+
+# PascalCase
+VariabelPertama
+
+variabel_pertama <- 5 + 5
+variabel_pertama
+
+variabel_pertama + 10
+
+variabel_kedua <- variabel_pertama + 10
+variabel_kedua
+
+variabel_ketiga <- variabel_kedua + variabel_pertama
+variabel_ketiga
+
+variabel_keempat = 5 + 10
+variabel_keempat
+
+20 + 10 -> variabel_kelima
